940da90a9050f8cecdcf8cb67447756cc6a262dd
OmAxiani0 <75031769+OmAximani0@users.noreply.github.com>
2024-02-29 15:40:36 +0530

    feat: upgrade spindler battery
    - service criteria changed from 2 years to 3 years
    
    Signed-off-by: OmAxiani0 <75031769+OmAximani0@users.noreply.github.com>

--- battery/spindlerbattery.py
+++ battery/spindlerbattery.py
@@ -8,7 +8,7 @@ class SpindlerBattery(Battery):
         self.current_date = current_date
         self.last_service_date = last_service_date
         self._serivce_threshold_date = last_service_date.replace(
-            last_service_date.year + 2
+            last_service_date.year + 3
         )
 
     def needs_service(self) -> bool:
